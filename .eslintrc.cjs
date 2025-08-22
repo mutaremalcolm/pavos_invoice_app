e6fad84e1e3fe1672d751589feaa2be46a993a41
“malcolmmutare” <mutarem@gmail.com>
2025-08-22 21:04:31 +0200

    setup: updated commit & linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -40,11 +40,10 @@ module.exports = {
         'src/generated/prisma/**',
     ],
     rules: {
-        qoutes: ['errors', 'single', { avoidEsscape: true, allowTemplateLiterals: false }],
-        semi: ['error', 'never'],
-        '@typescript-eslint/no-unused-vars': [1, { args: 'none' }],
+        qoutes: ['warn', 'single', { avoidEsscape: true, allowTemplateLiterals: false }],
+        semi: ['warn', 'never'],
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none'}],
         '@typescript-eslint/no-unused-expressions': 'warn',
-        '@typescript-eslint/no-this-alias': 'warn',
         'no-debugger': 'off',
         'prettier/prettier': [
             'warn',
@@ -52,29 +51,49 @@ module.exports = {
                 endOfLine: 'auto',
             },
         ],
-        'max-len': ['error', { code: 120, ignoreUrls: true, ignoreTemplateLiterals: true, ignoreStrings: true }],
-        'no-confusing-arrow': ['error', { allowParens: false }],
+        'max-len': ['error', { 
+            code: 120, 
+            ignoreUrls: true, 
+            ignoreTemplateLiterals: true, 
+            ignoreStrings: true,
+            ignoreComments: true 
+        }],
+        'no-confusing-arrow': ['error'],
         'no-mixed-operators': [
-            'error',
-            {
-                groups: [
-                    ['&', '|', '^', '~', '<<', '>>', '>>>'],
-                    ['==', '!=', '==', '!==', '>', '>=', '<', '<='],
-                    ['&&', '||'],
-                    ['in', 'instanceof'],
-                ],
+             {
+            groups: [
+                ['&', '|', '^', '~', '<<', '>>', '>>>'],
+                ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+                ['&&', '||'],
+                ['in', 'instanceof'],
+            ],
             },
-        ],
-        'no-tabs': ['error', { allowIndentationTabs: true }],
+        ], 
+        'no-tabs': ['error', { allowIndentation: true}], 
         'no-unexpected-multiline': 'error',
         // Disabling as conflicts with Prettier
         indent: 0,
         // Disabling as we are validating types with TypeScript not PropTypes
-        'react/react-in-jsx-scope': 0,
-        'react/prop-types': 0,
-        'react-hooks/rules-of-hooks': 0,
-        'react-hooks/exhaustive-deps': 0,
-        'react/display-name': 0,
+        'comma-dangle': 'off',
+        'object-curly-spacing': 'off',
+        'array-bracket-spacing': 'off',
+        
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        'react/no-children-prop': 'warn',
+        'react/no-unescaped-entities': 'warn',
+        'react/jsx-key': 'warn',
+        'react/jsx-no-duplicate-props': 'warn',
+        'react/jsx-no-undef': 'warn',
+        'react/no-deprecated': 'warn',
+        
+        'react-hooks/rules-of-hooks': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        
+        'prefer-const': 'warn',
+        'no-var': 'warn',
+        'no-duplicate-imports': 'warn',
     },
     settings: {
         react: {
